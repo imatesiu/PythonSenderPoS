b8da217c41d7c4d533925e20ba938336abe23d7c
Giorgio <imatesiu@gmail.com>
2018-06-08 10:29:19 +0200

    OLV interface added

--- interfaceOLV.py
+++ interfaceOLV.py
@@ -7,6 +7,9 @@ import time
 from base64 import b64encode
 from requests.auth import HTTPDigestAuth
 from requests.auth import HTTPBasicAuth
+import xml.etree.ElementTree
+from datetime import timedelta
+import xml.etree.ElementTree as ET
 import hashlib
 import re
 import datetime
@@ -47,24 +50,24 @@ tk_image = ImageTk.PhotoImage(pil_image2)
 class MiaApp:
 
 
-		
-		
+
+
 	def __init__(self, genitore):
 
-		
-		
+
+
 		self.mioGenitore = genitore
 		self.mioContenitore1 = Frame(genitore)
 		genitore.title("Python RCH Sender")
 		genitore.iconbitmap(r'favicon.ico')
-		
+
 		Label(self.mioContenitore1, text="Matricola: ").grid(row=0,column=0,pady=5,sticky=W)
-		
+
 		Label(self.mioContenitore1, text="IP del RT: ").grid(row=1,column=0,pady=5,sticky=W)
-		
+
 		Label(self.mioContenitore1, text="Intervallo: ").grid(row=2,pady=5,sticky=W)
 		v = StringVar()
-		v.set("88S25000026")
+		v.set("80I15000026")
 		self.e1 = Entry(self.mioContenitore1,textvariable=v)
 		v1 = StringVar()
 		v1.set("192.168.1.11")#192.168.1.146
@@ -75,21 +78,21 @@ class MiaApp:
 		self.e1.grid(row=0, column=1)
 		self.e2.grid(row=1, column=1)
 		self.e3.grid(row=2, column=1)
-		
+
 		Label(self.mioContenitore1, text="GT: ").grid(row=3,column=0,pady=5,sticky=W)
-		
+
 		Label(self.mioContenitore1, text="Z: ").grid(row=4,column=0,pady=5,sticky=W)
 		v3 = StringVar()
 		v3.set("00.00")
 		self.e4 = Entry(self.mioContenitore1,textvariable=v3)
-		
+
 		v4 = StringVar()
 		v4.set("0")
 		self.e5 = Entry(self.mioContenitore1,textvariable=v4)
-		
+
 		self.e4.grid(row=3, column=1)
 		self.e5.grid(row=4, column=1)
-		
+
 		self.prove = ("Termiche","Impermeabilita","Vibrazione","ImmunitaScaricheElettrostaticheESD","ImmunitaCampoElettromagneticoRadiofrequenza","ImmunitaTransitoriVelociEFTBURST","ImmunitaImpulsiSURGE","ImmunitaDisturbiCondottiContinuiRadiofrequenza","ImmunitaCampiMagneticiFrequenzaRete","ImmunitaBuchiInterruzioniTensione","VariazioniTensione","ProvaDurataBatteria","BatteriaSottoProtezioneSF","AlimentazioneBatteriaSenzaVincoloFiscale","Guastoemalfunzionamento")
 		self.cbp3 = Label(self.mioContenitore1, text='Prova: ')
 		self.cbp3.grid(row=5, column=0,pady=5)
@@ -97,37 +100,37 @@ class MiaApp:
 		self.cb3.current(1)  # set selection
 		self.cb3.grid(row=5, column=1)
 
-		
+
 		self.pulsante1 = Button(self.mioContenitore1,
 														command = self.pulsante1Premuto)
 		self.pulsante1.bind("<Button-1>", self.pulsante1Premuto_a)	### (1)
 		self.pulsante1.configure(text = "Start", background = "green")
 		self.pulsante1.grid(row=6, column=0,pady=25)
 		self.pulsante1.focus_force()
-		
-		
+
+
 		self.pulsante2 = Button(self.mioContenitore1,
 														command = self.pulsante2Premuto)
 		self.pulsante2.bind("<Return>", self.pulsante2Premuto)	### (1)
 		self.pulsante2.configure(text = "Pause", background = "yellow")
 		self.pulsante2.grid(row=6, column=1)
 		self.pulsante2.focus_force()
-		
+
 		self.pulsante3 = Button(self.mioContenitore1,
 														command = self.pulsante3Premuto)
 		self.pulsante3.bind("<Return>", self.pulsante3Premuto)	### (1)
 		self.pulsante3.configure(text = "Stop", background = "red")
 		self.pulsante3.grid(row=6, column=2)
 		self.pulsante3.focus_force()
-		
-		
+
+
 
 		self.pulsantee = Button(self.mioContenitore1,
 														command = self.pulsanteEPremuto)
 		self.pulsantee.bind("<Return>", self.pulsanteEPremuto_a)	### (1)
 		self.pulsantee.configure(text = "Chiudi", background = "red")
 		self.pulsantee.grid(row=8, column=0)
-		
+
 		self.pulsanteinfo = Button(self.mioContenitore1,
 														command = self.pulsanteInfoPremuto)
 		self.pulsanteinfo.bind("<Return>", self.pulsanteInfoPremuto)	### (1)
@@ -138,7 +141,7 @@ class MiaApp:
 
 
 		imglabel = Button(self.mioContenitore1, image=tk_image, command = self.pulsanteCNR)
-		imglabel.grid(row=9,column=1) 
+		imglabel.grid(row=9,column=1)
 		#imglabel.pack()
 		self.mioContenitore1.pack()
 
@@ -174,17 +177,17 @@ class MiaApp:
 			self.thread.dead = True
 		except AttributeError: print "Ops"
 		#self.mioGenitore.destroy()
-		
-		
+
+
 	def pulsanteEPremuto_a(self, evento):	### (3)
 		print "Gestore di eventi 'pulsante2Premuto_a' (un involucro)"
 
-		
+
 	def pulsanteInfoPremuto(self):	### (3)
 		print "Gestore di eventi pulsante Info Premuto"
 		self.send_getinfo(str(self.e1.get()))
-		
-		
+
+
 	def pulsanteEPremuto(self):	### (3)
 		print "Gestore di eventi pulsante Exit Premuto"
 		try:
@@ -192,8 +195,8 @@ class MiaApp:
 			self.thread.pause = False
 		except AttributeError: print "Ops"
 		exit(0)
-		
-		
+
+
 	def send_get(self,ip_server, url, param):
 		print "https://"+ip_server+"/"+url+param
 		try:
@@ -204,14 +207,14 @@ class MiaApp:
 			assert response.status_code == 200
 			return response.text
 		except ConnectionError: print "Problemi di Rete"
-		
-		
+
+
 	def send_init(self,matricola,gt,z,prova):
 		#https://localhost/v1/dispositivi/corrispettivi/init/80M08002493?grantot=28.8&desc=Termiche&z=33
 		url = "v1/dispositivi/corrispettivi/init/"
 		param = matricola+"?grantot="+gt+"&desc="+prova+"&z="+z
 		self.send_get(ip_server_rt,url,param)
-		
+
 	def pulsante1Premuto_a(self, evento):	### (3)
 		print "Gestore di eventi pulsante Start Premuto"
 		gt = self.e4.get()
@@ -220,11 +223,11 @@ class MiaApp:
 		self.thread = IlMioThread("AB120002", "aaa",self.e3.get(),self.e2.get(),self.e1.get())
 		self.thread.start()
 
-	
+
 	def send_stop(self,matricola):
 		url = "v1/dispositivi/corrispettivi/stop/"
 		self.send_get(ip_server_rt,url,matricola)
-		
+
 	def send_getinfo(self,matricola):
 		url = "v1/dispositivi/corrispettivi/info/"
 		self.send_get(ip_server_rt,url,matricola)
@@ -240,140 +243,72 @@ class IlMioThread (threading.Thread):
 		self.matricola = matr
 		self.dead = False
 		self.pause = False
-	
-	def createhash2(content):
-		sha256 = hashlib.sha256()
-		sha256.update(content)
-		return base64.b64encode(sha256.digest())
-	
-	def dateplus(self,date,ora):
-		text = str(date)+str(ora)
-		datet = datetime.datetime.strptime(text, '%d%m%Y%H:%M:%S')
-		end_date = datet + datetime.timedelta(days=1)
+
+	def xmltodate(self, xml):
+		root = ET.fromstring(xml)
+		a = (root.findall(".//extraInfo"))
+		text = a[0].text
+		datet = datetime.strptime(text, '%d%m%Y%H%M%S')
+		return datet
+		#print res
+
+	def dateplus(self, date):
+		end_date = date + timedelta(days=1)
 		return end_date.strftime('%d%m%Y')
-	
-	
-	def dateminus(self,date,ora):
-		text = str(date)+str(ora)
-		datet = datetime.datetime.strptime(text, '%d%m%Y%H:%M:%S')
-		end_date = datet - datetime.timedelta(days=1)
-		return end_date.strftime('%d%m%Y')
-	
-	def send_post(self,content, url, user, password):
-		print "https://"+self.set_ip_server+"/"+url
-		base64string = base64.encodestring('%s:%s' % (user, password)).replace('\n', '')
-		print base64string
-		pem = 'CASogeiTest.cer'
-		#response = requests.post('https://'+self.set_ip_server+'/'+url,data=content,headers={"Content-Type": "application/xml", "Authorization": "BASIC %s" % base64string  }, verify=False)
-		response = requests.post('https://'+self.set_ip_server+'/'+url,data=content,auth=HTTPBasicAuth(user, password),headers={"Content-Type": "application/xml"}, verify=False)
+
+
+	def createnewdata(self, xml):
+		da = self.xmltodate(xml)
+		r = self.dateplus(da)
+		print r
+		data4 = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\"><soap:Body><SetEcrDate><SETDATE><DATE>"+r+"</DATE><TIME>010000</TIME></SETDATE></SetEcrDate></soap:Body></soap:Envelope>"
+		return data4
+
+	def send_post(self, content, set_ip_apparato):
+		response = requests.post('http://'+set_ip_apparato+':80/oli_webservice.cgi',data=content,headers={"Content-Type": "text/xml"})
 		print response.text
 		assert response.status_code == 200
 		return response.text
-		
 
-	def createhash(self,content):
-		sha256 = hashlib.sha256()
-		sha256.update(content)
-		return sha256.hexdigest()
-	
+	def send_documentocommerciale(self, set_ip_apparato):
+		documentocommerciale = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\"><soap:Body><EcrTickets><CMD>...<E_SALE>..<OPTYPE>1</OPTYPE>....<BARCODETYPE></BARCODETYPE>..<NUMBER>1</NUMBER>..<LISTPRICE>1</LISTPRICE>..<SALETYPE>1</SALETYPE>..<DESCRIPTION></DESCRIPTION>..<AMOUNT>1</AMOUNT>..<QUANTITY_1></QUANTITY_1>..<QUANTITY_2></QUANTITY_2>..<QUANTITY_3></QUANTITY_3>..<QUANTITY_4></QUANTITY_4>..<M_QUANTITY></M_QUANTITY>..<S_QUANTITY></S_QUANTITY>..<RAEETYPE></RAEETYPE>..<RAEEVALUE></RAEEVALUE>.</E_SALE></CMD><CMD>...<E_PAYMENT>..<P_TYPE>1</P_TYPE>...<NUMBER>1</NUMBER>...<DESCRIPTION>PAGAMENTO</DESCRIPTION>...<AMOUNT></AMOUNT>....<CUSTOMERACCOUNT></CUSTOMERACCOUNT>...</E_PAYMENT></CMD></EcrTickets></soap:Body></soap:Envelope>"
+		self.send_post(documentocommerciale,set_ip_apparato)
 
+	def send_zfiscale(self, set_ip_apparato):
+		zfiscale = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\"><soap:Body><ExecuteReport>.<PRINTREP>1</PRINTREP>.<REPTYPE>1</REPTYPE>.<REPNUM>10</REPNUM>.</ExecuteReport></soap:Body></soap:Envelope>"
+		self.send_post(zfiscale,set_ip_apparato)
 
-		
-	def send_chiusura_cassa(self,user, password):
-		url = "ver1/api/richiesta/chiusura"
-		content = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Chiusura></Chiusura>"
-		self.send_post(content,url,user, password)
-	
-	def send_chiusura_server(self,user, password):
-		url = "ServerRT/ver1/api/richiesta/chiusura"
-		content = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Chiusura><ServerRT/></Chiusura>"
-		self.send_post(content,url,user, password)
-		
-	def send_forza_chiusura_server(self,user, password):
-		url = "ServerRT/ver1/api/richiesta/chiusura"
-		content = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Chiusura><ForzaChiusura/></Chiusura>"
-		self.send_post(content,url,user, password)	
-		
-	def send_configurazione_serverURL(self):
-		url = "ver1/api/configurazione/rete"
-		content = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><ConfigurazioneIP><URLAgenziaEntrate>https://192.168.1.146/v1/</URLAgenziaEntrate></ConfigurazioneIP>"
-		self.send_post(content, url, admin_user,admin_password)
-		
-	def send_apertura_cassa(self,user, password):
-		url = "ServerRT/ver1/api/richiesta/apertura"
-		content = "<AperturaPuntoCassa/>"
-		res = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><ConfermaAperturaPuntoCassa><UltimaChiusura>0000</UltimaChiusura></ConfermaAperturaPuntoCassa>"
-		response = self.send_post(content,url,user, password)
-		mydoc = minidom.parseString(response)
-		UltimaChiusura = mydoc.getElementsByTagName("UltimaChiusura")
-		return UltimaChiusura[0].firstChild.data
-	
-	def send_ric_token_cassa(self,user, password):
-		url = "ver1/api/richiesta/token"
-		content = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><RichiestaToken></RichiestaToken>"
-		res = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><RestituzioneToken><Token>98C6F5255688BB32393033323031383030303130303030303030303030303030</Token><MatricolaRT>88S25000010</MatricolaRT><PuntoCassa>AB120001</PuntoCassa><DataOra><Data>29032018</Data><Ora>17:47:26</Ora></DataOra></RestituzioneToken>"
-		response = self.send_post(content,url,user, password)
-		mydoc = minidom.parseString(response)
-		Tokens = mydoc.getElementsByTagName("Token")
-		Datas = mydoc.getElementsByTagName("Data")
-		Oras = mydoc.getElementsByTagName("Ora")
-		matricolaread = mydoc.getElementsByTagName("MatricolaRT")
-		if (not matricolaread[0].firstChild.data in self.matricola):
-			print "matricola del server diversa da quella impostata"
-			exit(0)
-		print Datas[0].firstChild.data
-		print Oras[0].firstChild.data
-		return Tokens[0].firstChild.data,Datas[0].firstChild.data,Oras[0].firstChild.data
-		
-	def send_documento(self,scontrino,cdcp,tk,user, password):
-		url = "ver1/api/invio/documento"
-		content = "<?xmlversion=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Documento><CCDCPrecedente>"+cdcp+"</CCDCPrecedente><PuntoCassa>"+user+"</PuntoCassa>"+scontrino+"<CCDC>"+tk+"</CCDC></Documento>"
-		#print content
-		cc = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Documento><CCDCPrecedente>"+cdcp+"</CCDCPrecedente><PuntoCassa>"+user+"</PuntoCassa>"+scontrino+"<CCDC>"+tk+"</CCDC></Documento>"
-		#res = "<?xmlversion=\"1.0\" encoding=\"UTF-8\"?><ConfermaDocumento><Data>29032018</Data><TotaleGiornaliero>12,20</TotaleGiornaliero><IdentificativoDocumento><NumeroAzzeramento>0001</NumeroAzzeramento><NumeroDocumento>0001</NumeroDocumento></IdentificativoDocumento></ConfermaDocumento>"
-		print cc
-		response = self.send_post(cc,url,user, password)
-		mydoc = minidom.parseString(response)
-		NumeroAzzeramentos = mydoc.getElementsByTagName("NumeroAzzeramento")
-		NumeroDocumentos = mydoc.getElementsByTagName("NumeroDocumento")
-		TotaleGiornalieros = mydoc.getElementsByTagName("TotaleGiornaliero")
-		print NumeroAzzeramentos[0].firstChild.data
-		print NumeroDocumentos[0].firstChild.data
-		print TotaleGiornalieros[0].firstChild.data
-	
-	
-	
-	def creascontrino(self,chiusura,data,ora,tkold,user, password):
-		scontrino = "<Scontrino><Data>"+data+"</Data><Ora>"+ora+"</Ora><NumeroDocumento>0001</NumeroDocumento><NumeroAzzeramento>"+str(chiusura).zfill(4)+"</NumeroAzzeramento><Dettagli><Vendita><Descrizione>Articolo16</Descrizione><Importo>10,00</Importo><Quantita>1</Quantita><PrezzoUnitario>10,00</PrezzoUnitario><CodiceIVA><Aliquota>10,00</Aliquota></CodiceIVA></Vendita></Dettagli><Dettagli><Pagamento><Descrizione>Contanti</Descrizione><Importo>10,00</Importo><Tipo>PC</Tipo></Pagamento></Dettagli><Dettagli><Pagamento><Descrizione>Elettronico</Descrizione><Importo>0,00</Importo><Tipo>PE</Tipo></Pagamento></Dettagli><Dettagli><Pagamento><Descrizione>Ticket</Descrizione><Importo>0,00</Importo><Tipo>TK</Tipo></Pagamento></Dettagli><Dettagli><Pagamento><Descrizione>NonRiscosso</Descrizione><Importo>0,00</Importo><Tipo>NR</Tipo></Pagamento></Dettagli><Dettagli><Pagamento><Descrizione>Resto</Descrizione><Importo>0,00</Importo><Tipo>RS</Tipo></Pagamento></Dettagli><Dettagli><Pagamento><Descrizione>Assegno</Descrizione><Importo>0,00</Importo><Tipo>AS</Tipo></Pagamento></Dettagli><Totale>10,00</Totale><CorrispettiviIVA><Importo>10,00</Importo><BaseImponibile>9,09</BaseImponibile><Imposta>0,91</Imposta><CodiceIVA><Aliquota>10,00</Aliquota></CodiceIVA></CorrispettiviIVA></Scontrino>"
-		cont = tkold+self.matricola+user+scontrino
-		tk = self.createhash(cont)
-		self.send_documento(scontrino,tkold,tk.upper(),user, password)
-	
+	def send_changedata(self, xml, set_ip_apparato):
+		response = self.send_post(xml,set_ip_apparato)
+		return response
 
-		
+	def create_send_new_data(self,  set_ip_apparato):
+		changedate = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\"><soap:Body><EcrTickets><CMD><GETDATE></GETDATE></CMD></EcrTickets></soap:Body></soap:Envelope>"
+		xml = self.send_changedata(changedate,set_ip_apparato)
+		d = str(xml)
+		dmsg = self.createnewdata(d)
+		self.send_post(dmsg,set_ip_apparato)
+		self.send_post(dmsg,set_ip_apparato)
+
 	def loop_HW(self,user,password,second):
-		self.send_configurazione_serverURL();
-		self.send_chiusura_cassa(user, password)
+		num = 0
 		while not self.dead:
-			kiusura = self.send_apertura_cassa(user, password)
-			z = str(int(kiusura)+1)
-			#print str(kiu)
-			#exit(0)
-			tokenp = self.send_ric_token_cassa(user, password)
-			self.creascontrino(z,tokenp[1],tokenp[2],tokenp[0],user, password)
-			self.send_chiusura_cassa(user, password)	
-			self.send_chiusura_server(user, password)	
+			if num == 1 or num == 40:
+					self.create_send_new_data(self.set_ip_server)
+					num = 2
+			self.send_documentocommerciale(self.set_ip_server)
+			self.send_zfiscale(self.set_ip_server)
 			time.sleep(float(second))
 			while(self.pause):
 				time.sleep(5)
-		
+
 
 
 
 	def run(self):
 		self.loop_HW(self.cassauser,self.password,self.second)
 		#self.creacassastart(self.cassauser,self.password)
-		
+
 
 
 
