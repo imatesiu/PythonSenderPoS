ce545fa05e5876441834e5b585c459090926d10a
Giorgio <imatesiu@gmail.com>
2018-11-12 20:13:02 +0100

    update serial sender

--- SerialRCH_sender.py
+++ SerialRCH_sender.py
@@ -36,10 +36,10 @@ def readers():
 	prev2 = {}
 	taxs = []
 	amount = 0
-	ndoc = 1
+	ndoc = 0
 	print "#####"
 	for line in spamReader:
-		print line
+		#print line
 		tipo = line[7]
 		if tipo == "DC":
 			importosenzasconto = line[0]
@@ -50,7 +50,44 @@ def readers():
 			percentualesconto = line[5]
 			valoresconto = line[6]
 			tipodocumento = line[7]
+			print "=R1/$"+str(float(importosenzasconto.replace(",","."))).replace(".","")
+			if len(percentualesconto)>1:
+				psconto = float(percentualesconto.replace(",","."))
+				print "=%/*"+str(psconto)
+			else:
+				if len(valoresconto)>1:
+					vsconto = float(valoresconto.replace(",","."))
+					print "=V/$"+str(vsconto).replace(".","")
+		if tipo == "P":
+			ticket = line[0]
+			bancomat = line[1]
+			Credito = line[2]
+			Assegni = line[3]
+			CartaC = line[4]
+			Contanti = line[5]
+			if len(ticket)>0:
+				itk = float(ticket.replace(",","."))
+				print "=T1${0:.0f}".format(itk*100)
+			if len(bancomat)>0:
+				itk = float(bancomat.replace(",","."))
+				print "=T2${0:.0f}".format(itk*100)
+			if len(Credito)>0:
+				itk = float(Credito.replace(",","."))
+				print "=T3${0:.0f}".format(itk*100)
+			if len(Assegni)>0:
+				itk = float(Assegni.replace(",","."))
+				print "=T4${0:.0f}".format(itk*100)
+			if len(CartaC)>0:
+				itk = float(CartaC.replace(",","."))
+				print "=T5${0:.0f}".format(itk*100)
+			if len(Contanti)>0:
+				itk = float(Contanti.replace(",","."))
+				print "=T${0:.0f}".format(itk*100)
+			print "=C"
+			ndoc+=1
+		if ndoc == 15:
 			exit(0)
+			#exit(0)Ticket;Bancomat;Credito;Assegni;CartaC;Contanti
 readers()
 
 
